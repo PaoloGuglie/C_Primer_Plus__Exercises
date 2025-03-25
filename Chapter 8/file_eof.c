895d0a54bacb0a34ead645d3c8aff879d56fa9f6
Your Name <paologugliel@gmail.com>
2025-03-25 20:45:17 +0100

    other_notes()

---
+++ Chapter 8/file_eof.c
@@ -0,0 +1,37 @@
+/*
+  This program opens a file and displays it.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int ch;
+  FILE * fp;
+  char fname[50];
+
+  printf("Enter the name of the file: ");
+  scanf("%s", fname);
+
+  // Open the file for reading
+  fp = fopen(fname, "r");
+
+  // If attempt failed
+  if (fp == NULL)
+  {
+    printf("Failed to open file. Bye!\n");
+    exit(1);  // quit program
+  }
+
+  // getc(fp) gets a character from the open file
+  while ((ch = getc(fp)) != EOF)
+  {
+    putchar(ch);
+  }
+
+  // Close the file
+  fclose(fp);
+
+  return 0;
+}
